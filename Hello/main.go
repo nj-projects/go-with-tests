25d86810a56b578ee0ee2a973efd9b0c6e6c95fa
nj-projects <jn@jamesnolan.me>
2025-07-12 13:09:02 +0100

    complete hello world

--- Hello/main.go
+++ Hello/main.go
@@ -1,18 +1,30 @@
 package main
 
-import (
-	"fmt"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 )
 
-const englishHelloPrefix = "Hello, "
-
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("world"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
